cb06ab908cd3bf5edcb3b7d6ac69e6578b2fe0c9
ha-young <hayeong28@naver.com>
2021-04-15 20:26:21 +0900

    [ADD] apply eslint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const logger = require('morgan');
 
 const indexRouter = require("./routes/index");
 
