fe58e418b8d24c58c88107976d285f2197c81d02
hyerichung <hyerichungonearth@gmail.com>
2021-04-16 16:27:57 +0900

    [ADD] /api/login endpoint, generate token for user

---
+++ utils/validateLoginData.js
@@ -0,0 +1,13 @@
+const Joi = require("joi");
+
+function validateLoginData(data) {
+  const schema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    imageUrl: Joi.string().required(),
+    name: Joi.string().required(),
+  });
+
+  return schema.validate({ ...data });
+}
+
+module.exports.checkLoginData = validateLoginData;
