ff74d507474e2aaefda7dea6ea952e22fc164ebb
hyerichung <hyerichungonearth@gmail.com>
2021-04-18 01:44:19 +0900

    [ADD] addToMyProjects in projectService.js

--- src/routes/api/projects.js
+++ src/routes/api/projects.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createProject } = require("../../services/projectService");
+const { createProject, addToMyProjects } = require("../../services/projectService");
 
 router.post("/", async (req, res, next) => {
   try {
     const project = req.body;
 
-    const { newProject, createProjectError } = await createProject(project);
+    const { newProject: { _id, creator }, createProjectError } = await createProject(project);
+    const { myProjects, error } = await addToMyProjects(creator, _id);
 
   } catch (error) {
     console.error(error);
