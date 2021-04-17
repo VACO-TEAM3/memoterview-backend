29bfc767211d2b88cab05f7d6cf29934950ad4d2
hyerichung <hyerichungonearth@gmail.com>
2021-04-18 01:44:19 +0900

    [ADD] addToMyProjects in projectService.js

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,4 +1,5 @@
 const Project = require("../models/Project");
+const Interviewer = require("../models/Interviewer");
 
 exports.createProject = async ({ title, filters, creator, participants }) => {
   try {
@@ -12,5 +13,19 @@ exports.createProject = async ({ title, filters, creator, participants }) => {
     return { newProject };
   } catch (error) {
     return { createProjectError: error };
+  }
+};
+
+exports.addToMyProjects = async (creator, _id) => {
+  try {
+    const myProjects = await Interviewer.findByIdAndUpdate(
+      creator,
+      { $push: { "myProjects": _id } },
+      { upsert: true, new: true }
+    );
+
+    return { myProjects };
+  } catch (error) {
+    return { addMyProjectsError: error };
   }
 };
