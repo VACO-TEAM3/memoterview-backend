f32ad7bd0240dd52bea0a86c07c8db861738ead4
Minjieun <chewchewchew3@gmail.com>
2021-04-19 03:19:30 +0900

    [ADD] socket for webRTC

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -3,11 +3,14 @@ const morgan = require("morgan");
 const mongooseLoader = require("./mongoose");
 const expressLoader = require("./express");
 const routerLoader = require("./router");
+const socketLoader = require("./socket");
 
-module.exports = function ({ app }) {
+module.exports = ({ app }) => {
   mongooseLoader();
 
   app.use(morgan("dev"));
 
   expressLoader({ app, routerLoader });
+
+  socketLoader({ app });
 };
