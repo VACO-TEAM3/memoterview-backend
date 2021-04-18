8954495a0f721910c676e3062fb3bb93b81053b5
hychoi <hayeong28@naver.com>
2021-04-19 04:28:45 +0900

    [FIX] change wrong required module IamAuthenticator to IamTokenManager

--- src/routes/api/speechToText.js
+++ src/routes/api/speechToText.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { IamAuthenticator } = require("ibm-watson/auth");
+const { IamTokenManager } = require("ibm-watson/auth");
 
 const router = express.Router();
 
 const serviceUrl = process.env.SPEECH_TO_TEXT_URL;
 
-const tokenManager = new IamAuthenticator({
+const tokenManager = new IamTokenManager({
   apikey: process.env.SPEECH_TO_TEXT_IAM_APIKEY || "<iam_apikey>",
 });
 
