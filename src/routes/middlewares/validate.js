c3ae95ab199fe2e666c89c4306e3d332944b8885
ha-young <hayeong28@naver.com>
2021-04-19 15:48:06 +0900

    [ADD] validate middleware

---
+++ src/routes/middlewares/validate.js
@@ -0,0 +1,19 @@
+const createError = require("http-errors");
+const Joi = require("joi");
+
+
+exports.default = function validate(schema, property) {
+  return (req, res, next) => {
+    const { error } = schema.validate(req[property]);
+    const valid = error == null;
+
+    if (valid) {
+      next();
+    } else {
+      const { details } = error;
+      const message = details.map((i) => i.message).join(",");
+
+      next(createError(422, { message }));
+    }
+  };
+};
