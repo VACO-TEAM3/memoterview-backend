f5aec2e1be277dd9511ba108781eca4453ca45cc
Minjieun <chewchewchew3@gmail.com>
2021-04-19 18:21:22 +0900

    [FIX] change socket for peer

--- src/loaders/socket.js
+++ src/loaders/socket.js
@@ -2,21 +2,56 @@ module.exports = ({ app }) => {
   const io = require("socket.io")();
 
   io.on("connection", (socket) => {
-    socket.on("join", ({ room }) => {
+    console.log(socket.id);
+    socket.on("join room", ({ room }) => {
       socket.join(room);
 
       const currentMembers = io.of("/").adapter.rooms.get(room);
       
-      io.sockets.emit("join", socket.id, Array.from(currentMembers));
+      if (currentMembers.has(room)) {
+        const memberCount = currentMembers.length;
+
+        if (memberCount === 5) {
+          socket.emit("room full");
+
+          return;
+        }
+
+        const antherUsers = currentMembers.filter((id) => id !== socket.id);
+
+        io.sockets.emit("all users", antherUsers);
+      }
+
+      socket.on("sending signal", (payload) => {
+        io.to(payload.userToSignal).emit("user joined", { signal: payload.signal, callerId: payload.callerId });
+      });
+
+      socket.on("returning signal", (payload) => {
+        io.to(payload.callerId).emit("receiving returned signal", { signal: payload.signal, id: socket.id });
+      });
+
+      socket.on("disconnect", ({ room }) => {
+        socket.leave(room);
+      });
     });
 
-    socket.on("signaling", (toId, message) => {
-      io.to(toId).emit("signaling", socket.id, message);
+    // socket.on("signaling", (toId, message) => {
+    //   io.to(toId).emit("signaling", socket.id, message);
+    // });
+
+    socket.on("icecandidate", (toId, message) => {
+      console.log(toId);
+      io.to(toId).emit("icecandidate", socket.id, message);
     });
 
-    socket.on("message", (toId, message) => {
-      console.log(toId, message);
-      io.to(toId).emit("message", socket.id, message);
+    socket.on("offer", (toId, message) => {
+      console.log(toId);
+      io.to(toId).emit("offer", socket.id, message);
+    });
+
+    socket.on("answer", (toId, message) => {
+      console.log(toId);
+      io.to(toId).emit("answer", socket.id, message);
     });
 
     socket.on("disconnect", () => {
