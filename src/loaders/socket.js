ef8249752989a65ebe80ce35f80b554060373bc9
Minjieun <chewchewchew3@gmail.com>
2021-04-19 20:22:15 +0900

    [ADD] working on socket

--- src/loaders/socket.js
+++ src/loaders/socket.js
@@ -2,32 +2,31 @@ module.exports = ({ app }) => {
   const io = require("socket.io")();
 
   io.on("connection", (socket) => {
-    console.log(socket.id);
     socket.on("join room", ({ room }) => {
       socket.join(room);
-
-      const currentMembers = io.of("/").adapter.rooms.get(room);
-      
-      if (currentMembers.has(room)) {
-        const memberCount = currentMembers.length;
+      console.log(room);
+      const members = Array.from(io.of("/").adapter.rooms.get(room));
+      console.log(members);
+      if (members) {
+        const memberCount = members.length;
 
         if (memberCount === 5) {
           socket.emit("room full");
 
           return;
         }
-
-        const antherUsers = currentMembers.filter((id) => id !== socket.id);
-
-        io.sockets.emit("all users", antherUsers);
+ 
+        const currentUsers = members.filter((id) => id !== socket.id);
+    
+        io.sockets.emit("all users", currentUsers);
       }
 
-      socket.on("sending signal", (payload) => {
-        io.to(payload.userToSignal).emit("user joined", { signal: payload.signal, callerId: payload.callerId });
+      socket.on("sending signal", ({ userToSignal, callerId, signal }) => {
+        io.to(userToSignal).emit("user joined", { signal, callerId });
       });
 
-      socket.on("returning signal", (payload) => {
-        io.to(payload.callerId).emit("receiving returned signal", { signal: payload.signal, id: socket.id });
+      socket.on("returning signal", ({ signal, callerId }) => {
+        io.to(callerId).emit("receiving returned signal", { signal, id: socket.id });
       });
 
       socket.on("disconnect", ({ room }) => {
@@ -35,28 +34,28 @@ module.exports = ({ app }) => {
       });
     });
 
-    // socket.on("signaling", (toId, message) => {
-    //   io.to(toId).emit("signaling", socket.id, message);
+    // // socket.on("signaling", (toId, message) => {
+    // //   io.to(toId).emit("signaling", socket.id, message);
+    // // });
+
+    // socket.on("icecandidate", (toId, message) => {
+    //   console.log(toId);
+    //   io.to(toId).emit("icecandidate", socket.id, message);
     // });
 
-    socket.on("icecandidate", (toId, message) => {
-      console.log(toId);
-      io.to(toId).emit("icecandidate", socket.id, message);
-    });
+    // socket.on("offer", (toId, message) => {
+    //   console.log(toId);
+    //   io.to(toId).emit("offer", socket.id, message);
+    // });
 
-    socket.on("offer", (toId, message) => {
-      console.log(toId);
-      io.to(toId).emit("offer", socket.id, message);
-    });
+    // socket.on("answer", (toId, message) => {
+    //   console.log(toId);
+    //   io.to(toId).emit("answer", socket.id, message);
+    // });
 
-    socket.on("answer", (toId, message) => {
-      console.log(toId);
-      io.to(toId).emit("answer", socket.id, message);
-    });
-
-    socket.on("disconnect", () => {
-      io.sockets.emit("user-left", socket.id);
-    });
+    // socket.on("disconnect", () => {
+    //   io.sockets.emit("user-left", socket.id);
+    // });
   });
 
   app.io = io;
