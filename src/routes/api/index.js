75dca49ab737e71ec25149a9b4954b1884a25103
Minjieun <chewchewchew3@gmail.com>
2021-04-23 00:44:12 +0900

    [ADD] update interviewee mongoose db

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,12 +4,10 @@ const router = express.Router();
 const loginRouter = require("../auth/login");
 const projectsRouter = require("./projects");
 const interviewerRouter = require("./interviewer");
-const speechToTextRouter = require("./speechToText");
 const authentication = require("../middlewares/authenticate");
 
 router.use("/api/login", loginRouter);
 router.use("/api/interviewers", authentication, interviewerRouter);
 router.use("/api/projects", authentication, projectsRouter);
-router.use("/api/speech-to-text", speechToTextRouter);
 
 module.exports = router;
