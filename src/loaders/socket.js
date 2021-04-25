c14554f2f6229ddf0543b7f028ad3900ed563734
Minjieun <chewchewchew3@gmail.com>
2021-04-25 15:48:34 +0900

    [DELETE] mongoose instance by review

--- src/loaders/socket.js
+++ src/loaders/socket.js
@@ -47,13 +47,12 @@ module.exports = ({ app }) => {
     socket.on("disconnect", () => {
       socket.broadcast.emit("userLeft");
       const roomID = socketToRoom[socket.id];
-
-      const leftUsers = rooms[roomID].members.filter(
+      const leftUsers = rooms[roomID]?.members.filter(
         (member) => member.socketID !== socket.id
       );
 
-      if (leftUsers.length !== 0) {
-        if (rooms[roomID].members) {
+      if (leftUsers?.length !== 0) {
+        if (rooms[roomID]) {
           rooms[roomID].members = leftUsers;
         }
       } else {
