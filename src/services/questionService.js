c3fc6036e42e01998f8225d01c860f73425f55cd
Minjieun <chewchewchew3@gmail.com>
2021-04-28 17:29:01 +0900

    [FIX] find to aggregation in getQuestions service

--- src/services/questionService.js
+++ src/services/questionService.js
@@ -2,8 +2,13 @@ const Question = require("../models/Question");
 
 exports.getQuestions = async ({ category }) => {
   try {
-    const categorizedQuestions = await Question.find({ category }, { title: 1 });
-
+    const categorizedQuestions = await Question.aggregate(
+      [
+        { $match: { category } },
+        { $sample: { size: 10 } },
+      ]
+    );
+    console.log(categorizedQuestions);
     return categorizedQuestions;
   } catch (error) {
     return { getQuestionError: error };
